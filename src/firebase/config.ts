dec819d1469928ac89a0d5aeb09ed283e00cf2bf
KANDUKURI KIRAN KUMAR <kandukurik6@gmail.com>
2026-02-08 13:15:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3189231952-6cc11",
+  "appId": "1:780439029618:web:3146a4cc1e5a68c5d2ffeb",
+  "apiKey": "AIzaSyAiar04fumQn7FnfFwSauaEl3EcJVIDPPQ",
+  "authDomain": "studio-3189231952-6cc11.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "780439029618"
+};
