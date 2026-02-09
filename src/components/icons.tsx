341fb9283d08a872913cd3b18bc9a04867c31690
KANDUKURI KIRAN KUMAR <kandukurik6@gmail.com>
2026-02-09 12:55:15 +0000

    B should be in hexagon shape

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,22 +12,16 @@ export function AppIcon(props: SVGProps<SVGSVGElement>) {
           d="M33.9999 0.900024L6.49994 16.5V47.1L33.9999 57.9L61.4999 47.1V16.5L33.9999 0.900024Z"
           fill="#374151"
         />
-        <path
-          d="M34 5.4L9.80005 18.5V45.1L34 53.4L58.2 45.1V18.5L34 5.4Z"
+        <text
+          x="34"
+          y="33"
+          fontFamily="Inter, sans-serif"
+          fontSize="36"
+          fontWeight="700"
           fill="white"
-        />
-        <path
-          d="M33.9999 9.90002L13.1999 21.2V42.8L33.9999 49.6L54.7999 42.8V21.2L33.9999 9.90002Z"
-          fill="#374151"
-        />
-        <path
-          d="M34 14.4L16.6 24V39.6L34 45.1L51.4 39.6V24L34 14.4Z"
-          fill="white"
-        />
-        <path
-          d="M34 18.9L20 26.7V36.9L34 42.4L48 36.9V26.7L34 18.9Z"
-          fill="#374151"
-        />
+          textAnchor="middle"
+          dominantBaseline="middle"
+        >B</text>
       </g>
       
       <text 
