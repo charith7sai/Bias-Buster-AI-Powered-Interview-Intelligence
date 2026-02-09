31720e9316879c250bce5fc6a42c9b525eab8c55
KANDUKURI KIRAN KUMAR <kandukurik6@gmail.com>
2026-02-09 12:43:45 +0000

    set this as my app logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,14 +7,14 @@ export function AppIcon(props: SVGProps<SVGSVGElement>) {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
       {...props}
     >
-      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M21.65 12L18.31 19H5.69L2.35 12L5.69 5H18.31L21.65 12ZM18.31 12L16.19 17.5H7.81L5.69 12L7.81 6.5H16.19L18.31 12ZM14.2 9L13.2 7.5H10.8L9.8 9H8V15H9.8L10.8 16.5H13.2L14.2 15H16V9H14.2ZM14 10.5V13.5H13.2L12.2 12L13.2 10.5H14Z"
+      />
     </svg>
   );
 }
